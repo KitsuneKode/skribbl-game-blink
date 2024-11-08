cd65b58192a710ace526878683cf2dac1fc38582
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-11-08 20:42:15 +0530

    added new routes

---
+++ app/create-game/page.tsx
@@ -0,0 +1,7 @@
+export default async function CreateGame() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-4xl font-bold">Create Game</h1>
+    </div>
+  );
+}
