097e7134d853ba9c190338afdafc8eb5e0ac5745
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-11-08 21:12:58 +0530

    fixig route

--- app/play-game/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function PlayGame() {
-  return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className="text-4xl font-bold">Play Game</h1>
-    </div>
-  );
-}
