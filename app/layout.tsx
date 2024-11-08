cfecf269760c575e9a1339c4f1bb50ab16e33d13
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-11-08 21:03:23 +0530

    action.josn fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>The Rock (1996)</title>
+        <meta property="og:title" content="The Rock" />
+        <meta property="og:type" content="video.movie" />
+        <meta
+          property="og:url"
+          content="https://www.imdb.com/title/tt0117500/"
+        />
+        <meta
+          property="og:image"
+          content="https://ia.media-imdb.com/images/rock.jpg"
+        />
+        ...
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
